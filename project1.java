735a6f3c9e6628735a5422c2341915439e03754e
KANG8181 <166059174+KANG8181@users.noreply.github.com>
2024-04-04 15:20:50 -0400

    Add files via upload

---
+++ project1.java
@@ -0,0 +1,16 @@
+package com.example.project;
+
+public class project1 {
+
+	public static void main(String[] args) {
+		// System.out.println("Welcome to week 01");
+		int a,b,sum;
+		a=2;
+		b=3;
+		sum=a+b;
+System.out.println("sum is");
+
+	}
+
+	
+}
